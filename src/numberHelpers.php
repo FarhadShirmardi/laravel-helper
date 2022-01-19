cc8f4aa425017c6701c7ac912db4510b8a4aa3f8
farhad <shirmardi7@gmail.com>
2022-01-19 12:14:49 +0330

    - roundDown function.

--- src/numberHelpers.php
+++ src/numberHelpers.php
@@ -4,6 +4,6 @@ if (!function_exists('roundDown')) {
     function roundDown($number, $precision)
     {
         $multiplier = 10 ** $precision;
-        return floor($number * $precision) / $precision;
+        return floor($number * $multiplier) / $multiplier;
     }
 }
